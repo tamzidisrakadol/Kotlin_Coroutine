fede83a728d22e78b3ff72f6ff1bae415904f089
Tamzid Israk Adol <tamzidisrak12@gmail.com>
2025-02-15 04:05:21 +0600

    Learn about zip and combine operator.

--- app/src/main/java/com/example/kotlincoroutine/FLow/operators/Transform.kt
+++ app/src/main/java/com/example/kotlincoroutine/FLow/operators/Transform.kt
@@ -5,19 +5,65 @@ import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.flatMapConcat
 import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.flatMapMerge
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.mapLatest
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.transform
+import kotlinx.coroutines.flow.zip
 import kotlinx.coroutines.runBlocking
 import java.lang.System.currentTimeMillis
 
 fun main() {
-    transformExample()
+    combineExample()
 }
+
+
+private fun combineExample(){
+    /* perform a computation that depends on the most recent values of the corresponding
+    flows and to recompute it whenever any of the upstream flows emit a value.
+     The corresponding family of operators is called combine
+    *Independent Emissions: The flows being combined can emit values independently of each other.
+    * */
+
+
+
+    val nums = (1..3).asFlow().onEach { delay(100) }
+    val strs = flowOf("one", "two", "three").onEach { delay(200) }
+    val startTime = System.currentTimeMillis()
+    runBlocking(){
+        nums.combine(strs){int, strings->
+            "$int -> $strings"
+        }.collect{
+            println("$it at ${System.currentTimeMillis() - startTime} ms from start")
+        }
+    }
+}
+
+
+private fun zipExample(){
+    /*  flows have a zip operator that combines the corresponding values of two flows
+    *   data print out sequentially
+    *   wait for the first one to printout 2nd
+    */
+
+
+    val nums = (1..3).asFlow().onEach { delay(1000) }
+    val strs = flowOf("one", "two", "three")
+    runBlocking(){
+        nums.zip(strs){int, strings->
+            "$int -> $strings"
+        }.collect{
+            println(it)
+        }
+    }
+
+}
+
 
 private fun concatExample() {
     /* there will be 2 flows converting into 1. But the twist is if there is any delay it will complete the code sequentially
@@ -82,7 +128,10 @@ private fun requestFlow1(i: Int): Flow<String> = flow {
 }
 
 private fun transformExample(){
-    /* Using the transform operator, we can emit arbitrary values an arbitrary number of times.*/
+    /* Using the transform operator, we can emit arbitrary values an arbitrary number of times.
+    * code work as sequentially
+    * wait for the first one to complete before starting to collect the next
+    */
 
 
     runBlocking(Dispatchers.IO){
