c10bfc8a7caa0b6149d36906a2314246379f78ad
Tamzid Israk Adol <tamzidisrak12@gmail.com>
2025-02-25 07:16:59 +0600

    Learn about Anonymous class

---
+++ app/src/main/java/com/example/kotlincoroutine/AllClass/Anonymous.kt
@@ -0,0 +1,42 @@
+package com.example.kotlincoroutine.AllClass
+
+interface Receiver{
+     fun onReceive(data: String){}
+     fun onLost(error: Exception){}
+}
+
+
+open class Animal{
+
+    open fun makeSound(){
+
+    }
+}
+
+
+fun main() {
+
+    val dog = object : Animal(){
+
+        override fun makeSound() {
+            super.makeSound()
+        }
+
+    }
+
+
+
+    val anonymousClass = object : Receiver {
+        override fun onReceive(data: String) {
+            TODO("Not yet implemented")
+        }
+
+        override fun onLost(error: Exception) {
+            TODO("Not yet implemented")
+        }
+
+    }
+
+    anonymousClass.onReceive("")
+
+}
